929ee866e8cdb32170bc7d5869c9e5c3395f545d
royidudkman <106233840+royidudkman@users.noreply.github.com>
2024-03-21 18:47:56 +0200

    register works

--- app/src/main/java/com/example/fitplan/UI/login_register/LoginFragment.kt
+++ app/src/main/java/com/example/fitplan/UI/login_register/LoginFragment.kt
@@ -1,16 +1,31 @@
 package com.example.fitplan.UI.login_register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.example.fitplan.R
 import com.example.fitplan.databinding.FragmentLoginBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
+
+    private val _mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val mAuth get() = _mAuth
+
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,12 +33,20 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.loginBtn.setOnClickListener {
+            val navController = NavHostFragment.findNavController(this)
+            navController.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/fitplan/UI/login_register/RegisterFragment.kt
+++ app/src/main/java/com/example/fitplan/UI/login_register/RegisterFragment.kt
@@ -1,15 +1,26 @@
 package com.example.fitplan.UI.login_register
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
+import com.example.fitplan.R
 import com.example.fitplan.databinding.FragmentRegisterBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class RegisterFragment : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+
+    private val _mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+    private val mAuth get() = _mAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +28,10 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        binding.registerBtn.setOnClickListener{
+            RegisterFunc()
+        }
 
 
 
@@ -26,6 +41,33 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    fun RegisterFunc() {
+        val email = binding.emailTextInput.editText?.text.toString().trim()
+        val password = binding.passwordTextInput.editText?.text.toString().trim()
+
+        val navController = NavHostFragment.findNavController(this)
+
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            // TODO Show message
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity(),
+                OnCompleteListener<AuthResult?> { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(requireContext(),"Registered Successfuly", Toast.LENGTH_LONG).show()
+                        val user : FirebaseUser? = mAuth.currentUser
+                        //TODO Write data to Database
+                        //writeUserDataToDatabase(user.uid, name, email, phone)
+                        navController.navigate(R.id.action_registerFragment_to_loginFragment)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        //TODO Show message
+                    }
+                })
     }
 
     override fun onDestroyView() {
