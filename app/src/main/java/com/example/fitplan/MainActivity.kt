75eaeb81185f75417f2a71249da0bea8f4b30cc1
royidudkman <106233840+royidudkman@users.noreply.github.com>
2024-03-21 15:21:01 +0200

    stam Commit

--- app/src/main/java/com/example/fitplan/MainActivity.kt
+++ app/src/main/java/com/example/fitplan/MainActivity.kt
@@ -7,17 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //ROYI WAS HERE
-        //Amit was here
-        //yarin was here
-        //royi2
-        //amit2
-        //royi 3
-        //royi 4
-        //stam
-        //hara
-        //aaa
-        //aaaaa
-        //its da boy
     }
 }
